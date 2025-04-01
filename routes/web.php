9eb508ece337f3176c16b50492abcf014f41afd5
erwinbrown <erwinbkfire@gmail.com>
2025-04-01 09:17:54 -0500

    master

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
      echo "RH Management";
+});
+
+
+Route::get('/email', function () {
+     Mail::raw('Mensaje de prueba de RH Management', function(Message $message){
+
+          $message->to('teste@gmail.com')
+          ->subject('Bienvenido a RH Management')
+          ->from('rh@rhmanagement.com');
+     });
+
+     echo "Correo enviado existosamente";
 });
